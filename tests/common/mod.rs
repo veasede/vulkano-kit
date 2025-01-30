3f560f492bb06580a11a931ecf1c87b501756682
siaeyy <iso5374@hotmail.com>
2025-01-31 00:59:50 +0300

    test: init

---
+++ tests/common/mod.rs
@@ -0,0 +1,23 @@
+use std::sync::{Arc, LazyLock, Mutex, MutexGuard};
+
+use vulkano::VulkanLibrary;
+use vulkano::instance::Instance;
+
+
+#[derive(Debug, Default)]
+pub struct Common {
+    pub library: Option<Arc<VulkanLibrary>>,
+    pub instance: Option<Arc<Instance>>,
+}
+
+static _COMMON: LazyLock<Mutex<Common>> = LazyLock::new(|| Mutex::new(Common{..Default::default()}));
+
+fn common<'a>() -> MutexGuard<'a, Common> {
+    _COMMON.lock().unwrap()
+}
+
+pub fn set_library<'a>(lib: Arc<VulkanLibrary>) { common::<'a>().library = Some(lib); }
+pub fn set_instance<'a>(ins: Arc<Instance>) { common::<'a>().instance = Some(ins); }
+
+pub fn get_library<'a>() -> Option<Arc<VulkanLibrary>> { common().library.clone() }
+pub fn get_instance<'a>() -> Option<Arc<Instance>> { common().instance.clone() }
