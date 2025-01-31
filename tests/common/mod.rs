e6465a9ffe4715b3b6935e8e981ae34e46ba9f7c
siaeyy <iso5374@hotmail.com>
2025-01-31 07:34:16 +0300

    lint(test/common)

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -3,9 +3,8 @@ mod playground;
 
 use std::sync::{Arc, LazyLock, Mutex, MutexGuard};
 
-use vulkano::VulkanLibrary;
 use vulkano::instance::Instance;
-
+use vulkano::VulkanLibrary;
 
 #[derive(Debug, Default)]
 pub struct Common {
@@ -13,14 +12,26 @@ pub struct Common {
     pub instance: Option<Arc<Instance>>,
 }
 
-static _COMMON: LazyLock<Mutex<Common>> = LazyLock::new(|| Mutex::new(Common{..Default::default()}));
+static _COMMON: LazyLock<Mutex<Common>> = LazyLock::new(|| {
+    Mutex::new(Common {
+        ..Default::default()
+    })
+});
 
 fn common<'a>() -> MutexGuard<'a, Common> {
     _COMMON.lock().unwrap()
 }
 
-pub fn set_library<'a>(lib: Arc<VulkanLibrary>) { common::<'a>().library = Some(lib); }
-pub fn set_instance<'a>(ins: Arc<Instance>) { common::<'a>().instance = Some(ins); }
+pub fn set_library<'a>(lib: Arc<VulkanLibrary>) {
+    common::<'a>().library = Some(lib);
+}
+pub fn set_instance<'a>(ins: Arc<Instance>) {
+    common::<'a>().instance = Some(ins);
+}
 
-pub fn get_library<'a>() -> Option<Arc<VulkanLibrary>> { common().library.clone() }
-pub fn get_instance<'a>() -> Option<Arc<Instance>> { common().instance.clone() }
+pub fn get_library<'a>() -> Option<Arc<VulkanLibrary>> {
+    common().library.clone()
+}
+pub fn get_instance<'a>() -> Option<Arc<Instance>> {
+    common().instance.clone()
+}
