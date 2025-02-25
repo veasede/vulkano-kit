0a76c9012f1897ca9c73ee36b0682603d804897e
siaeyy <iso5374@hotmail.com>
2025-02-25 06:03:45 +0300

    feat(playground): upgrade image manipulation case

--- playground/src/cases/image_manipulation.rs
+++ playground/src/cases/image_manipulation.rs
@@ -1,21 +1,60 @@
 use super::{CaseReturn, Utils};
 
-use vulkano::command_buffer::CommandBufferUsage;
+use vulkano::{
+    buffer::BufferUsage,
+    command_buffer::{ClearColorImageInfo, CommandBufferUsage, CopyImageToBufferInfo},
+    format::{ClearColorValue, Format},
+    image::ImageUsage,
+    memory::allocator::MemoryTypeFilter,
+};
 
 use vulkano_kit::*;
 
+use buffer::{create_basic_allocator, create_basic_buffer_from_iter, empty_iter};
 use command::{create_command_buffer_allocator, create_primary_builder};
+use image::create_2d_image;
 
 
 pub fn case(utils: &Utils) -> CaseReturn {
+    let allocator = create_basic_allocator(utils.device.clone());
+
+    let image = create_2d_image(
+        allocator.clone(),
+        (1024, 1024),
+        Format::R8G8B8A8_UNORM,
+        ImageUsage::TRANSFER_DST | ImageUsage::TRANSFER_SRC,
+    )
+    .unwrap();
+
+    let _empty = empty_iter::<u8>(1024 * 1024 * 4);
+    let buffer = create_basic_buffer_from_iter(
+        _empty,
+        allocator.clone(),
+        BufferUsage::TRANSFER_DST,
+        MemoryTypeFilter::PREFER_HOST | MemoryTypeFilter::HOST_RANDOM_ACCESS,
+    )
+    .unwrap();
+
     let command_buffer_allocator = create_command_buffer_allocator(utils.device.clone());
 
-    let command_buffer_builder = create_primary_builder(
+    let mut command_buffer_builder = create_primary_builder(
         command_buffer_allocator.clone(),
         utils.queue_family_index,
         CommandBufferUsage::OneTimeSubmit,
     )
     .expect("Failed to create command buffer builder");
+
+    command_buffer_builder
+        .clear_color_image(ClearColorImageInfo {
+            clear_value: ClearColorValue::Float([0.0, 0.0, 1.0, 1.0]),
+            ..ClearColorImageInfo::image(image.clone())
+        })
+        .unwrap()
+        .copy_image_to_buffer(CopyImageToBufferInfo::image_buffer(
+            image.clone(),
+            buffer.clone(),
+        ))
+        .unwrap();
 
     (command_buffer_builder, None)
 }
