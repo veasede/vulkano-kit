dc8638c97437b78dd3cf5ef390a413bcd3a3914c
siaeyy <iso5374@hotmail.com>
2025-02-25 05:59:53 +0300

    feat(buffer): buffer from iter

--- src/buffer.rs
+++ src/buffer.rs
@@ -49,3 +49,34 @@ where
         data,
     )
 }
+
+pub fn create_basic_buffer_from_iter<T, I>(
+    iter: I,
+    allocator: BasicAllocator,
+    usage: BufferUsage,
+    memory_type_filter: MemoryTypeFilter,
+) -> Result<Subbuffer<[T]>, Validated<AllocateBufferError>>
+where 
+    T: BufferContents,
+    I: IntoIterator<Item = T>,
+    I::IntoIter: ExactSizeIterator,
+{
+    Buffer::from_iter(
+        allocator,
+        BufferCreateInfo {
+            usage,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter,
+            ..Default::default()
+        },
+        iter,
+    )
+}
+
+pub fn empty_iter<T: From<u8>>(
+    size: u32,
+) -> impl IntoIterator<Item = T, IntoIter: ExactSizeIterator> {
+    (0..size).map(|_| T::from(0))
+}
